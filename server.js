efd1382d61dd88f262859864b3819d073b17cd0b
Auxilia Vinnarasi <auxiliavinnarasi@gmail.com>
2021-10-11 11:47:50 +0530

    commit

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if(process.env.NODE_ENV ==="production")
     app.use("/",express.static("client/build"))   
 
     app.get("*",(req,res)=>{         //what is entry point for client
-        res.sendFile(path.resolve(__dirname,"/client/build/index.html"))
+        res.sendFile(path.resolve(__dirname,"client/build/index.html"))
     })
 }
 
